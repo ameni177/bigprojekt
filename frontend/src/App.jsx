bf8d8ae1117bcdfe871a841bda313b3296078825
ameni <ameni.kallel@edu.techstarter.de>
2024-07-02 12:57:00 +0200

    db

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Register from './Register'
-import Signin from './Signin'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  
+    const [data, setData] = useState([]);
 
-  return (
-    <>
-      <Register/>
-      <Signin/>
-     
-    
-    </>
-  )
+    useEffect(() => {
+        axios.get('http://localhost:3001/api/data')
+            .then(response => {
+                setData(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching data: ' + error);
+            });
+    }, []);
+
+    useEffect(() => {
+        console.log(data); // This will log the updated data whenever it changes
+    }, [data]); // Run this effect whenever `data` changes
+
+    return (
+        <div>
+            <h1>MySQL Data from AWS RDS</h1>
+            <ul>
+                {data.map(item => (
+                    <li key={item.id}>{item.user}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
-export default App
+export default App;
