1dab3cbcdfb8b678c20b0e974415a96263499fd5
Shavar2743 <markus.shavar@web.de>
2024-07-05 11:55:21 +0200

    impressum + layout profil added

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,40 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Footer.css';
-import ReactDOM from 'react-dom';
-//import Impressum from './impressum.jsx';
-//import Info from './datenschutz.jsx';
-//import Karriere from './kontakt.jsx';
-const Footer = () => {
+
+function DarkMode() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    const savedMode = localStorage.getItem('darkMode');
+    if (savedMode === 'enabled') {
+      setDarkMode(true);
+      document.body.classList.add('dark-mode');
+    }
+  }, []);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    if (!darkMode) {
+      document.body.classList.add('dark-mode');
+      localStorage.setItem('darkMode', 'enabled');
+    } else {
+      document.body.classList.remove('dark-mode');
+      localStorage.setItem('darkMode', 'disabled');
+    }
+  };
+
+  return (
+    <div className="DarkMode">
+      <Footer onToggleTheme={toggleDarkMode} />
+    </div>
+  );
+}
+
+const Footer = ({ onToggleTheme }) => {
   return (
     <footer className="footer">
       <div className="footer-content">
-         <p>&copy; {new Date().getFullYear()} Konferenz-Service IT GmbH <p></p>Alle Rechte vorbehalten.</p>
-         
+        <p className="text" >&copy; {new Date().getFullYear()} Ihr Unternehmen. Alle Rechte vorbehalten.</p>
+        <button className="dark-mode-button" onClick={onToggleTheme}>Dark Mode</button>
       </div>
     </footer>
   );
 };
 
-//export default Footer;
-//<ul className="footer-links">12
-//<li><a href="#">Impressum</a></li>
-//<li><a href="#">Datenschutz</a></li>
-//<li><a href="#">Kontakt</a></li>
-//</ul>
-// const Footer = ({ onToggleTheme }) => {
-//   return (
-//     <footer className="footer">
-//       <div className="footer-content">
-//         <p>&copy; {new Date().getFullYear()} Ihr Unternehmen. Alle Rechte vorbehalten.</p>
-//          <ul className="footer-links">
-//           <li><a href="#">Impressum</a></li>
-//          <li><a href="#">Datenschutz</a></li>
-//           <li><a href="#">Kontakt</a></li>
-//          </ul>
-//         <button onClick={onToggleTheme}>Dark Mode</button>
-//        </div>
-//    </footer>
-//   );
-// };
-
-export default Footer;
+export default DarkMode;
